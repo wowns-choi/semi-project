79e84618ae51cb87f2b265252d92211d7d3fd9d9
wowns-choi <wowns590@naver.com>
2024-04-23 14:48:14 +0900

    first commit

---
+++ semiProject/src/main/java/com/project/semi/SemiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.semi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiProjectApplication.class, args);
+	}
+
+}
---
+++ semiProject/src/test/java/com/project/semi/SemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.semi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
