720a2f41106670dd0e159fecd67cc8f1f548fdc0
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-04-26 14:29:57 +0900

    docs:"게시판수정"

--- semiProject/src/main/java/com/project/semi/board/controller/BoardController.java
+++ semiProject/src/main/java/com/project/semi/board/controller/BoardController.java
@@ -5,10 +5,14 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.project.semi.board.model.service.BoardService;
+import com.project.semi.member.model.dto.Member;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,19 +30,22 @@ public class BoardController {
 			@RequestParam(value="cp", required=false, defaultValue="1") int cp,
 			Model model) {
 		
-		log.debug("현재페이지 : " + cp);
-		
-		System.out.println(cp);
-		
-		
 		// 자유 게시판 조회 서비스 호출 후 결과 반환
 		Map<String, Object> map = service.selectBoardList(cp);
 		
 		model.addAttribute("pagination", map.get("pagination"));
 		model.addAttribute("boardList", map.get("boardList"));
 		
-//		System.out.println(map.get("pagination"));
-		
 		return "board/boardList";
+	}
+	
+	@GetMapping("all/{boardNo:[0-9]+}")
+	public String boardDetail(@PathVariable("boardNo") int boardNo,
+			Model model,
+			RedirectAttributes ra,
+			@SessionAttribute("loginMember") Member loginMember) {
+		
+		
+		return "";
 	}
 }
